a1a1e1e022f4082919780321edc1a2b8ca87e4e4
a.carolina.clemente <a.carolina.clemente@WIN-274L9CMFBS>
2020-10-12 21:51:41 -0300

    Commite inicial

---
+++ Projeto_API/Services/IProductsService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Projeto_API.Model;
+
+
+namespace Projeto_API.Services
+{
+    public interface IProductsService
+    {
+         List<Product> GetProducts();
+
+         Product AddProduct(Product productItem);
+
+         Product UpdateProduct(string id, Product productItem);
+
+         string DeleteProduct(string id);
+    }
+}
